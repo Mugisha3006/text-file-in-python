8889354d9f1a25a026793a5d0a899a70af53f695
mugisha <mohamugi@gmail.com>
2024-03-07 17:29:37 +0300

    read and write text-file

---
+++ document.py
@@ -0,0 +1,10 @@
+f_p = "document.txt"
+
+f = open(f_p, "a")
+
+f.write("A common way to classify communication is by whether information is exchanged between humans, members of other species, or non-living entities such as computers.")
+
+f.close()
+
+print(f)
+
---
+++ read.py
@@ -0,0 +1,7 @@
+file_path = "read.txt"
+
+file = open(file_path, "r")
+
+file.close()
+
+print(file)
